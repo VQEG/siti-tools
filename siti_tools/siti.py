0eed9631e10fe4eea54f1aff6193e7a744eafa7b
Lukas Krasula <lkrasula@netflix.com>
2021-07-23 18:20:45 -0700

    Implementation of eotf_1886, eotf_inv_srgb, eotf_hlg, and oetf_pq

--- siti_tools/siti.py
+++ siti_tools/siti.py
@@ -115,13 +115,60 @@ def ti(
         return (frame_data - previous_frame_data).std()
 
 
-def eotf_1886(frame_data: np.ndarray, gamma: float = DEFAULT_GAMMA) -> np.ndarray:
-    # TODO implement
+def eotf_1886(
+    frame_data: np.ndarray,
+    gamma: float = DEFAULT_GAMMA,
+    l_min: float = 0.0,
+    l_max: float = 1.0,
+) -> np.ndarray:
+    """
+    Electro-optical transfer function defined in ITU-T Rec. BT.1886
+
+    Args:
+        frame_data: pixel values expected in the range [0, 1]
+        gamma: exponent
+        l_min: minimum luminance
+        l_max: maximum luminance
+    Note:
+        As the mapping to display luminance happens outside of this function, l_min and l_max are set to 0.0 and 1.0,
+        respectively. If you change these values while using this function in the context of SI/TI calculation, your
+        output will be wrong. You can however change the values when using this function in a standalone fashion.
+
+    Returns:
+        frame_data: pixel values in the range [0, 1]
+
+    """
+    frame_data = np.maximum(frame_data, 0.0)
+    frame_data = np.minimum(frame_data, 1.0)
+
+    a = np.power(np.power(l_max, 1.0 / gamma) - np.power(l_min, 1.0 / gamma), gamma)
+    b = np.power(l_min, 1.0 / gamma) / (
+        np.power(l_max, 1.0 / gamma) - np.power(l_min, 1.0 / gamma)
+    )
+
+    frame_data = a * np.power(np.maximum(frame_data + b, 0), gamma)
+
     return frame_data
 
 
 def eotf_inv_srgb(frame_data: np.ndarray) -> np.ndarray:
-    # TODO implement
+    """
+    Inverse sRGB electro-optical transfer function
+
+    Args:
+        frame_data: pixel values expected in the range [0, 1]
+
+    Returns:
+        frame_data: pixel values in the range [0, 1]
+
+    """
+    frame_data = np.maximum(frame_data, 0.0)
+    frame_data = np.minimum(frame_data, 1.0)
+
+    frame_data = (frame_data <= 0.04045) * frame_data / 12.92 + (
+        frame_data > 0.04045
+    ) * np.power((frame_data + 0.055) / 1.055, 2.4)
+
     return frame_data
 
 
@@ -152,7 +199,27 @@ def apply_display_model(
 
 
 def oetf_pq(frame_data: np.ndarray) -> np.ndarray:
-    # TODO implement
+    """
+    Perceptual quantizer opto-electrical transfer function defined in ITU-T Rec. BT.2100
+
+    Args:
+        frame_data: pixel values expected to be in the physical luminance between 0 and 10,000 cd/m^2
+
+    Returns:
+        frame_data: pixel values in the range [0, 1]
+
+    """
+
+    m = 78.84375
+    n = 0.1593017578125
+    c1 = 0.8359375
+    c2 = 18.8515625
+    c3 = 18.6875
+    lm1 = np.power(10000.0, n)
+    lm2 = np.power(frame_data, n)
+
+    frame_data = np.power((c1 * lm1 + c2 * lm2) / (lm1 + c3 * lm2), m)
+
     return frame_data
 
 
@@ -161,7 +228,30 @@ def eotf_hlg(
     l_min: float = DEFAULT_L_MIN_HDR,
     l_max: float = DEFAULT_L_MAX_HDR,
 ) -> np.ndarray:
-    # TODO implement
+    """
+    Hybrid log-gamma opto-electrical transfer function defined in ITU-T Rec. BT.2100
+
+    Args:
+        frame_data: pixel values expected to be in the range [0, 1]
+
+    Returns:
+        frame_data: pixel values in the physical luminance up to 10,000 cd/m^2
+
+    """
+    frame_data = np.maximum(frame_data, 0.0)
+    frame_data = np.minimum(frame_data, 1.0)
+
+    a = 0.17883277
+    b = 0.02372241
+    c = 1.00429347
+    gamma = 1.2
+
+    frame_data = (frame_data <= 0.5) * (np.power(frame_data, 2.0) / 3.0) + (
+        frame_data > 0.5
+    ) * (np.exp((frame_data - c) / a) - b)
+
+    frame_data = (l_max - l_min) * np.power(frame_data, gamma - 1.0) + l_min
+
     return frame_data
 
 
