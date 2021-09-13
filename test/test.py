b2f934c52064845daa490c927099d60723268026
Werner Robitza <werner.robitza@gmail.com>
2021-09-13 14:20:26 +0200

    update test suite

--- test/test.py
+++ test/test.py
@@ -29,31 +29,92 @@ import os
 from typing import Dict, List
 from numpy import NaN
 import pytest
+import requests
+from tqdm import tqdm
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from siti_tools.file import read_container  # noqa: E402
-from siti_tools.siti import SiTiCalculator  # noqa: E402
+from siti_tools.siti import (
+    SiTiCalculator,
+    ColorRange,
+    EotfFunction,
+    HdrMode,
+)  # noqa: E402
 
 
 def pytest_generate_tests(metafunc):
-    idlist = []
-    argvalues = []
-    argnames = []
-    for scenario in metafunc.cls.scenarios:
-        idlist.append(scenario[0])
-        items = scenario[1].items()
-        argnames = [x[0] for x in items]
-        argvalues.append([x[1] for x in items])
-    if "input_file" in metafunc.fixturenames and "ground_truth" in metafunc.fixturenames:
-        metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
+    """
+    This function automatically populates the tests based on the contents of the 'BASIC_SCENARIOS' variable below.
+    """
+    test_siti_main_scenario_ids = []
+    test_siti_main_argvalues = []
+    test_siti_main_argnames = []
+
+    test_siti_calculator_scenario_ids = []
+    test_siti_calculator_argvalues = []
+    test_siti_calculator_argnames = []
+
+    if metafunc.function.__name__ == "test_siti_main_functions":
+        for scenario in metafunc.cls.BASIC_SCENARIOS:
+            # e.g. ('basic testing', {'input_file': 'test.mp4', 'ground_truth': 'test.csv'})
+            scenario_id, scenario_params = scenario
+            test_siti_main_scenario_ids.append(scenario_id)
+            scenario_kwargs = scenario_params.items()
+            test_siti_main_argnames = [x[0] for x in scenario_kwargs]
+            test_siti_main_argvalues.append([x[1] for x in scenario_kwargs])
+
+        metafunc.parametrize(
+            test_siti_main_argnames,
+            test_siti_main_argvalues,
+            ids=test_siti_main_scenario_ids,
+            scope="class",
+        )
+
+    elif metafunc.function.__name__ == "test_siti_calculator":
+        for scenario in metafunc.cls.EXTENDED_SCENARIOS:
+            scenario_id, scenario_params = scenario
+            test_siti_calculator_scenario_ids.append(scenario_id)
+            scenario_kwargs = scenario_params.items()
+            test_siti_calculator_argnames = [x[0] for x in scenario_kwargs]
+            test_siti_calculator_argvalues.append([x[1] for x in scenario_kwargs])
+
+        metafunc.parametrize(
+            test_siti_calculator_argnames,
+            test_siti_calculator_argvalues,
+            ids=test_siti_calculator_scenario_ids,
+            scope="class",
+        )
 
 
 class TestSiti:
-    scenarios = [
+    BASIC_SCENARIOS = [
         ("basic testing", {"input_file": "test.mp4", "ground_truth": "test.csv"}),
-        ("foreman", {"input_file": "foreman_cif.y4m", "ground_truth": "foreman_cif.csv"}),
+        (
+            "foreman",
+            {"input_file": "foreman_cif.y4m", "ground_truth": "foreman_cif.csv"},
+        ),
         # add further tests here
+    ]
+
+    EXTENDED_SCENARIOS = [
+        # TODO
+        # these require further instantiation via the SiTiCalculator class, with further meta variables
+        (
+            "FourPeople_480x270_60.y4m",
+            {
+                "input_file": "https://media.xiph.org/video/aomctc/test_set/a5_270p/FourPeople_480x270_60.y4m",
+                "ground_truth": "FourPeople_480x270_60.y4m",
+                "siti_calculator_kwargs": {"hdr_mode": HdrMode.HDR10},
+            },
+        )
+        # (
+        #     "CosmosCaterpillar_2048x858p24_hdr10",
+        #     {
+        #         "input_file": "https://media.xiph.org/video/aomctc/test_set/hdr2_2k/CosmosCaterpillar_2048x858p24_hdr10.y4m",
+        #         "ground_truth": "CosmosCaterpillar_2048x858p24_hdr10.csv",
+        #     },
+        # )
     ]
 
     @staticmethod
@@ -72,14 +133,82 @@ class TestSiti:
                 ret.append({"si": float(si), "ti": float(ti)})
         return ret
 
-    def test_siti(self, input_file: str, ground_truth: str):
+    @staticmethod
+    def _try_download_file(remote_path: str):
+        """Download a remote file via HTTP/S
+
+        Args:
+            remote_path (str): The remote URL
+        """
+        local_path = os.path.join(
+            os.path.dirname(__file__), "videos", os.path.basename(remote_path)
+        )
+        print(f"Downloading to {local_path} ...")
+        response = requests.get(remote_path, stream=True)
+        total_size_in_bytes = int(response.headers.get("content-length", 0))
+        block_size = 1024 * 1024  # 1 MiB
+        progress_bar = tqdm(total=total_size_in_bytes, unit="iB", unit_scale=True)
+        with open(local_path, "wb") as local_file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                local_file.write(data)
+        progress_bar.close()
+
+    def test_siti_calculator(
+        self,
+        input_file: str,
+        ground_truth: str,
+        siti_calculator_kwargs,
+    ):
+        input_file_path = os.path.join(os.path.dirname(__file__), "videos", input_file)
+        ground_truth_path = os.path.join(
+            os.path.dirname(__file__), "ground_truth", ground_truth
+        )
+
+        if not os.path.isfile(input_file_path) and input_file.startswith("https:"):
+            TestSiti._try_download_file(input_file)
+
+        gt = TestSiti._read_ground_truth(ground_truth_path)
+
+        calculator = SiTiCalculator(**siti_calculator_kwargs)
+        si, ti, _ = calculator.calculate(input_file)
+
+        frame_cnt = 1
+
+        for siti_results, gt_data in zip(list(zip(si, ti)), gt):
+            print(f"Comparing frame {frame_cnt}")
+
+            si_value, ti_value = siti_results
+
+            print(si_value, ti_value)
+            print(gt_data)
+
+            assert pytest.approx(si_value, 0.01) == gt_data["si"]
+
+            if frame_cnt == 1:
+                assert ti_value is None
+            else:
+                assert pytest.approx(ti_value, 0.01) == gt_data["ti"]
+
+            frame_cnt += 1
+
+    def test_siti_main_functions(self, input_file: str, ground_truth: str):
         """
         Test the raw SI/TI calculation functions.
 
         Note: This does not test the entire integration with the display model/HDR conversion.
+
+        Args:
+            input_file (str): file name of the input file or HTTP path in case of remote video
+            ground_truth (str): file name of ground truth CSV file
         """
         input_file_path = os.path.join(os.path.dirname(__file__), "videos", input_file)
-        ground_truth_path = os.path.join(os.path.dirname(__file__), "ground_truth", ground_truth)
+        ground_truth_path = os.path.join(
+            os.path.dirname(__file__), "ground_truth", ground_truth
+        )
+
+        if not os.path.isfile(input_file_path) and input_file.startswith("https:"):
+            TestSiti._try_download_file(input_file)
 
         frame_generator = read_container(input_file_path)
         gt = TestSiti._read_ground_truth(ground_truth_path)
