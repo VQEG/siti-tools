6c5751f2e1e22b8bf8b2fa6aaebd155d72b15f8e
Werner Robitza <werner.robitza@gmail.com>
2023-06-27 20:46:25 +0200

    update test decode function

--- test/generate_ffmpeg_sources.sh
+++ test/generate_ffmpeg_sources.sh
@@ -31,3 +31,10 @@ ffmpeg -y -f lavfi -i color=white:size=320x240 -f lavfi -i color=black:size=320x
 
 # checkerboard pattern, 8x8px, 10-bit
 ffmpeg -y -f lavfi -i color=white:size=320x240 -f lavfi -i color=black:size=320x240 -filter_complex "[0:v][1:v]blend=all_expr='if(eq(mod(floor(X/32),2),mod(floor(Y/32),2)),A,B)',scale=in_range=limited:out_range=full,signalstats,metadata=print" -frames:v 3 -pix_fmt yuv420p10le -strict -1 videos/checkerboard-8x8-10bpp.y4m
+
+# various width/height combinations
+ffmpeg -y -f lavfi -i testsrc=size=1920x1080 -filter:v "scale=in_range=limited:out_range=full,signalstats,metadata=mode=print" -frames:v 1 -pix_fmt yuv420p videos/test-1080p.y4m
+ffmpeg -y -f lavfi -i testsrc=size=1280x720 -filter:v "scale=in_range=limited:out_range=full,signalstats,metadata=mode=print" -frames:v 1 -pix_fmt yuv420p videos/test-720p.y4m
+ffmpeg -y -f lavfi -i testsrc=size=854x480 -filter:v "scale=in_range=limited:out_range=full,signalstats,metadata=mode=print" -frames:v 1 -pix_fmt yuv420p videos/test-480p.y4m
+ffmpeg -y -f lavfi -i testsrc=size=640x360 -filter:v "scale=in_range=limited:out_range=full,signalstats,metadata=mode=print" -frames:v 1 -pix_fmt yuv420p videos/test-360p.y4m
+ffmpeg -y -f lavfi -i testsrc=size=426x240 -filter:v "scale=in_range=limited:out_range=full,signalstats,metadata=mode=print" -frames:v 1 -pix_fmt yuv420p videos/test-240p.y4m
--- test/test_decode.py
+++ test/test_decode.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 #
 # Verify output of PyAV with limited/full range content
+import glob
+
 import av
 import numpy as np
+
 
 # generate with:
 # ffmpeg -y -f lavfi -i testsrc=size=320x240 -filter:v "scale=in_range=limited:out_range=limited,signalstats,metadata=mode=print" -frames:v 3 -pix_fmt yuv420p videos/limited-range.y4m
@@ -19,7 +22,6 @@ def useful_array(plane, bytes_per_pixel=1):
 def decode_and_print(input_file, bitdepth=8):
     print(input_file)
     container = av.open(input_file)
-    breakpoint()
 
     if bitdepth == 8:
         datatype = np.uint8
@@ -31,10 +33,13 @@ def decode_and_print(input_file, bitdepth=8):
     for idx, frame in enumerate(container.decode(video=0)):
         frame_data = (
             np.frombuffer(frame.planes[0], datatype)
+            .reshape(frame.height, frame.width)
+            .astype("int")
             # useful_array(frame.planes[0])
-            .reshape(frame.height, frame.width).astype("int")
         )
-        print(f"frame {idx} -- min: {np.min(frame_data)}, max: {np.max(frame_data)}")
+        print(
+            f"frame {idx} ({frame.width}x{frame.height}) -- min: {np.min(frame_data)}, max: {np.max(frame_data)}"
+        )
     print()
 
 
@@ -46,4 +51,7 @@ def decode_and_print(input_file, bitdepth=8):
 
 # decode_and_print("videos/checkerboard-8x8-10bpp-limited.y4m", bitdepth=10)
 
-decode_and_print("videos/checkerboard-8x8-10bpp.y4m", bitdepth=10)
+# decode_and_print("videos/checkerboard-8x8-10bpp.y4m", bitdepth=10)
+
+for video in glob.glob("videos/test-*p.y4m"):
+    decode_and_print(video, bitdepth=8)
